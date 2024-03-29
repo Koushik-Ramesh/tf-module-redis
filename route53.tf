2a16f0614733cc243f6284d532db07fb99b2449d
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-29 12:32:43 +0530

    Autopush

--- route53.tf
+++ route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "redis_dns" {
   name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
-  records = [aws_elasticache_cluster.redis.num_cache_nodes.0.address]
+  records = [aws_elasticache_cluster.redis.cache_nodes.0.address]
 }
